b33828fc9afdd110b0f96698a3abfeff2a381d9e
Thomas Liske <liske@ibh.de>
2012-09-10 17:35:14 +0200

    Begin reengineering Virtual PC/Hyper-V detection stuff.

--- perl/lib/ImVirt/VMD/Microsoft.pm
+++ perl/lib/ImVirt/VMD/Microsoft.pm
@@ -4,7 +4,7 @@
 #   Thomas Liske <liske@ibh.de>
 #
 # Copyright Holder:
-#   2009 - 2012 (C) IBH IT-Service GmbH [http://www.ibh.de/]
+#   2012 (C) IBH IT-Service GmbH [http://www.ibh.de/]
 #
 # License:
 #   This program is free software; you can redistribute it and/or modify
@@ -22,17 +22,22 @@
 #   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
 
-package ImVirt::VMD::VirtualPC;
+package ImVirt::VMD::Microsoft;
 
 use strict;
 use warnings;
-use constant PRODUCT => '|VirtualPC';
+use constant PRODUCT => '|Microsoft';
+use constant HYPERV => 'Hyper-V';
+use constant VIRTUALPC => 'VirtualPC';
 
 use ImVirt;
 use ImVirt::Utils::blkdev;
 use ImVirt::Utils::dmidecode;
 use ImVirt::Utils::dmesg;
 use ImVirt::Utils::helper;
+use ImVirt::Utils::pcidevs;
+use ImVirt::Utils::procfs;
+use ImVirt::Utils::kmods;
 
 ImVirt::register_vmd(__PACKAGE__);
 
@@ -58,17 +63,45 @@ sub detect($) {
 	ImVirt::inc_pts($dref, $p, IMV_VIRTUAL, PRODUCT);
     }
     else {
-	ImVirt::dec_pts($dref, IMV_PTS_MAJOR, IMV_VIRTUAL, PRODUCT);
+	ImVirt::dec_pts($dref, IMV_PTS_NORMAL, IMV_VIRTUAL, PRODUCT);
     }
 
     # Check helper output
     if(my $hlp = helper('hyperv')) {
-	ImVirt::inc_pts($dref, IMV_PTS_MAJOR, IMV_VIRTUAL, PRODUCT, split(/\s+/, $hlp));
+	ImVirt::inc_pts($dref, IMV_PTS_MAJOR, IMV_VIRTUAL, PRODUCT);
+    }
+
+    # Check /proc/bus/pci/devices
+    my %pcidevs = pcidevs_get();
+    foreach my $addr (keys %pcidevs) {
+	if(${$pcidevs{$addr}}{'device'} eq 'Hyper-V virtual VGA') {
+	    ImVirt::inc_pts($dref, IMV_PTS_MAJOR, IMV_VIRTUAL, PRODUCT, HYPERV);
+	}
+	elsif(${$pcidevs{$addr}}{'device'} eq 'DECchip 21140 [FasterNet]') {
+	    ImVirt::inc_pts($dref, IMV_PTS_MINOR, IMV_VIRTUAL, PRODUCT);
+	}
+	elsif(${$pcidevs{$addr}}{'device'} eq 'Microsoft Corporation Device 0007') {
+	    ImVirt::inc_pts($dref, IMV_PTS_MINOR, IMV_VIRTUAL, PRODUCT, VIRTUALPC);
+	}
+    }
+
+    # Check /proc/irq/7/hyperv
+    ImVirt::inc_pts($dref, IMV_PTS_MAJOR, IMV_VIRTUAL, PRODUCT, HYPERV)
+	if(procfs_isdir('irq/7/hyperv'));
+
+    # Look for Hyper-V modules
+    $p = kmods_match_used(
+	'^hv_(vmbus|storvsc|netvsc|utils)$' => IMV_PTS_NORMAL,
+    );
+    if($p > 0) {
+	ImVirt::inc_pts($dref, $p, IMV_VIRTUAL, PRODUCT);
     }
 }
 
 sub pres() {
-    return (PRODUCT);
+    return (
+	PRODUCT.HYPERV
+    );
 }
 
 1;
